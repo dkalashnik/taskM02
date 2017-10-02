65382ed3c0d85016d7d2221a8cb35310205cf4a2
Dmitry Kalashnik <dkalashnik@mirantis.com>
2017-10-02 13:27:29 +0400

    Add simple SUT emulator

---
+++ test_service.py
@@ -0,0 +1,20 @@
+from random import choice
+
+from flask import Flask, jsonify, request
+
+
+results = ["PASSED", "FAILED", "ERROR", "UNKNOWN"]
+
+
+app = Flask(__name__)
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def index():
+    print request.json
+        return jsonify({"status": choice(results)})
+
+
+	if __name__ == '__main__':
+	    app.run(debug=True, port=8787)
+
